056462500fd8898cba11ba262e21416283129fef
YouWillSun <44455690+YouWillSun@users.noreply.github.com>
2019-06-25 10:51:04 +0800

    完善图片上传逻辑

--- pages/postingImg/postingImg.js
+++ pages/postingImg/postingImg.js
@@ -5,10 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 存放文本框输入的值
+    describeWord: '',
     // 图片路径
-    imgPath:[],
+    imgPath: [],
     // base64图片编码数组
-    ImgBase64:[]
+    ImgBase64: []
   },
 
   /**
@@ -17,29 +19,39 @@ Page({
   onLoad: function (options) {
 
   },
+
+  // 获取用户输入的文本框的值
+  describeWord(e) {
+    var that = this;
+    // 把获取到的值，绑定到data相应的变量里
+    that.setData({
+      describeWord: e.detail.value
+    });
+  },
+
   // 从本地相册或相机选择图片
-  chooseImg(){
+  chooseImg() {
     var that = this;
     // 选择趣图
     wx.chooseImage({
       count: 4,// 最大选择数量四张，默认9张
-      sizeType: ['original','compressed'],// 原图或压缩图，默认二者皆有
-      sourceType: ['album','camera'],// 相册图片还是相机拍摄，默认二者皆有
-      success: (result)=>{
+      sizeType: ['original', 'compressed'],// 原图或压缩图，默认二者皆有
+      sourceType: ['album', 'camera'],// 相册图片还是相机拍摄，默认二者皆有
+      success: (result) => {
         var tempFilePath = result.tempFilePaths;// 获取图片路径[多个图片获取到的就是一个字符串数组]
         // 把字符串改成json对象数组
         var imgPathArray = []
-        for(let n in tempFilePath){
-          imgPathArray.push({path:tempFilePath[n]})
+        for (let n in tempFilePath) {
+          imgPathArray.push({ path: tempFilePath[n] })
         }
         // 给data里对应的数组赋值
         that.setData({
-          imgPath:imgPathArray
+          imgPath: imgPathArray
         });
         var fileManager = wx.getFileSystemManager();
         // 先定义一个数组，用来存放编码后的图片
         var base64 = [];
-        for(let i in tempFilePath){// 循环对多张图片进行格式编码
+        for (let i in tempFilePath) {// 循环对多张图片进行格式编码
           fileManager.readFile({
             filePath: tempFilePath[i],// 图片路径
             encoding: 'base64',// 编码格式
@@ -52,10 +64,10 @@ Page({
         }
         // 把base64编码设置到data对应的数组里
         that.setData({
-          ImgBase64:base64
+          ImgBase64: base64
         })
       },
-      fail: ()=>{
+      fail: () => {
         wx.showToast({
           title: '图片错误',
           duration: 1500
@@ -65,8 +77,64 @@ Page({
   },
 
   //上传图片
-  uploadImg(){
+  uploadImg() {
     var that = this;
+    // 判断数据不能为空
+    if (that.data.describeWord == "") {
+      wx.showToast({
+        title: '趣图描述为空',
+        duration: 1500
+      });
+    } else if (that.data.ImgBase64.length <= 0) {
+      wx.showToast({
+        title: '还未选择趣图',
+        duration: 1500
+      });
+    } else {
+      // 从本地存储中获取到guid的数据
+      wx.getStorage({
+        key: 'Guid',
+        // 获取成功，则发送请求
+        success(res) {
+          wx.request({
+            url: "https://www.barteam.cn/ApiRoot/UpLoadFunnyImg/UpLoadFunnyImgs",
+            data: {
+              "guid": res.data,
+              "imgList": that.data.ImgBase64,
+              "imgDecribe": that.data.describeWord
+            },
+            header: { 'content-type': 'application/json', 'cookie': wx.getStorageSync('sessionid') },
+            method: 'POST',
+            dataType: 'json',
+            responseType: 'text',
+            success: (result) => {
+              var res = result.data;
+              if (res.status == 'ok') {
+                wx.showToast({
+                  title: res.mess,
+                  duration: 1500
+                });
+              } else {
+                wx.showToast({
+                  title: '未知错误',
+                  duration: 1500
+                });
+              }
+            },
+            // 请求失败，展示失败信息
+            fail: (result) => {
+              var res = result.data;
+              if (res.status == 'fail') {
+                wx.showToast({
+                  title: res.mess,
+                  duration: 1500
+                });
+              }
+            }
+          });
+        }
+      });
+    }
   },
 
   /**
